f6d8a4518e69485a94a0636f3648143f5bfd9092
yousif alnasser <yousif.97a@gmail.com>
2025-02-27 01:52:26 +0300

    solved

--- test_api.py
+++ test_api.py
@@ -2,3 +2,10 @@
 
 # TODO: paste the code from Kitt's instructions
 
+import requests
+
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+print(response)
+city = response[0]
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
